d076a521e44b871bd956b02196cd8e5ccf489b87
Adam Lipian <adamlipian2723@gmail.com>
2024-06-18 02:32:55 +0200

    feat: Main class

---
+++ src/main/java/com/lipian/raycaster/Raycaster.java
@@ -0,0 +1,11 @@
+package com.lipian.raycaster;
+
+import com.lipian.raycaster.screen.*;
+
+public class Raycaster {
+    public static void main(String[] args) {
+        Frame frame = new Frame();
+        frame.add(new Panel());
+        frame.setVisible(true);
+    }
+}
