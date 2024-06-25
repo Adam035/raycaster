8533d89d94b9f5318eb822faf00ba559b82ae7c2
Adam Lipian <adamlipian2723@gmail.com>
2024-06-25 16:43:44 +0200

    feat(screen): Full screen

--- src/main/java/com/lipian/raycaster/screen/Frame.java
+++ src/main/java/com/lipian/raycaster/screen/Frame.java
@@ -1,15 +1,32 @@
 package com.lipian.raycaster.screen;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Frame extends JFrame {
     public Frame() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
         setTitle("Raycaster");
         ImageIcon icon = new ImageIcon("src/main/resources/images/icon.png");
         setIconImage(icon.getImage());
         pack();
         setResizable(false);
+        addEscapeKeyListener();
+    }
+
+    private void addEscapeKeyListener() {
+        String escapeKey = "ESCAPE";
+        KeyStroke escapeKeyStroke = KeyStroke.getKeyStroke(escapeKey);
+        InputMap inputMap = getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+        ActionMap actionMap = getRootPane().getActionMap();
+        inputMap.put(escapeKeyStroke, escapeKey);
+        actionMap.put(escapeKey, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
     }
 }
